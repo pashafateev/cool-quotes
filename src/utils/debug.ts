47450dc8c1af5ab75434745101e4df9e346a7907
Pasha Fateev <pasha@pashafateev.com>
2025-07-11 16:01:55 -0700

    fix build errors

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -2,13 +2,13 @@ const isDebugMode = () => {
     return process.env.NEXT_PUBLIC_DEBUG_MODE === 'true' || process.env.NODE_ENV === 'development';
 };
 
-export const debugLog = (...args: any[]) => {
+export const debugLog = (...args: unknown[]) => {
     if (isDebugMode()) {
         console.log(...args);
     }
 };
 
-export const debugError = (...args: any[]) => {
+export const debugError = (...args: unknown[]) => {
     if (isDebugMode()) {
         console.error(...args);
     }
