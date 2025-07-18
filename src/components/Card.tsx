dc6e8605b1b4ed8e1d6cda0b8c59f7d55d169685
Pasha Fateev <pasha@pashafateev.com>
2025-07-18 15:23:44 -0700

    card styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -43,50 +43,43 @@ const Card = ({
           backgroundColor: color,
           scale,
           top: `calc(-5vh + ${i * 25}px)`,
+          display: "flex",
+          flexDirection: "column",
+          position: "relative",
+          height: "500px",
+          width: "1000px",
+          borderRadius: "25px",
+          padding: "50px",
+          transformOrigin: "top",
         }}
       >
         <Box
           sx={{
             display: "flex",
             flexDirection: "column",
-            position: "relative",
-            height: "500px",
-            width: "1000px",
-            borderRadius: "25px",
-            padding: "50px",
-            backgroundColor: color,
-            transformOrigin: "top",
-            top: `calc(-5vh + ${i * 25}px)`,
+            height: "100%",
+            marginTop: "50px",
+            width: "100%",
           }}
         >
           <Box
             sx={{
+              flex: 1,
               display: "flex",
-              flexDirection: "column",
-              height: "100%",
-              marginTop: "50px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Quote quote={q} onWordClick={onWordClick} />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
               width: "100%",
             }}
           >
-            <Box
-              sx={{
-                flex: 1,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Quote quote={q} onWordClick={onWordClick} />
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "flex-end",
-                width: "100%",
-              }}
-            >
-              <Author quote={q} />
-            </Box>
+            <Author quote={q} />
           </Box>
         </Box>
       </motion.div>
